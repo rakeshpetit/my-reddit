f97cc90133ba188e3cc5fae727bdb961971c58fb
Rakesh Arunachalam <rakesh@thoughtbot.com>
2021-03-05 13:55:03 +0000

    Get all the inserted posts in the DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,10 +6,13 @@ import mikroOrmConfig from "./mikro-orm.config"
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig)
     await orm.getMigrator().up()
-    const post = orm.em.create(Post, {
-        title: 'first post'
-    })
-    await orm.em.persistAndFlush(post)
+    // const post = orm.em.create(Post, {
+    //     title: 'first post'
+    // })
+    // await orm.em.persistAndFlush(post)
+
+    const posts = await orm.em.find(Post, {})
+    console.log(posts)
 }
 
 main().catch(error => {
