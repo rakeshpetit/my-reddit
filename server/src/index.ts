79047d8f8896c8ac302d52288dc55df2b3bcc8e0
Rakesh Arunachalam <rakesh@thoughtbot.com>
2021-03-05 16:00:57 +0000

    Add express and graphql middlewares to the project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,19 @@
 import { MikroORM } from "@mikro-orm/core"
+import express from "express"
 import { Post } from "./entities/Post"
 import mikroOrmConfig from "./mikro-orm.config"
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig)
   await orm.getMigrator().up()
-  // const post = orm.em.create(Post, {
-  //   title: 'first post'
-  // })
-  // await orm.em.persistAndFlush(post)
 
-  const posts = await orm.em.find(Post, {})
-  console.log(posts)
+  const app = express()
+  app.get('/', (req, res) => {
+    res.send('yo man!')
+  })
+  app.listen(4000, () => {
+    console.log('Server started on port 4000')
+  })
 }
 
 main().catch(error => {
