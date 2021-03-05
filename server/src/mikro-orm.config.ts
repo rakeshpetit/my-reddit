12484bfa1e13b9eebe1a6eae5b0e8049684c216a
Rakesh Arunachalam <rakesh@thoughtbot.com>
2021-03-05 13:43:23 +0000

    Run migrations to create missing tables

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,8 +1,13 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from '@mikro-orm/core'
+import path from 'path'
 
 export default {
+    migrations: {
+        path: path.join(__dirname, './migrations'),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
     entities: [Post],
     dbName: 'reddit',
     type: 'postgresql',
