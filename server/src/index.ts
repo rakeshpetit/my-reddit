8acdb86ac35abaaeda4f14015118b4074023ab1b
Rakesh Arunachalam <rakesh@thoughtbot.com>
2021-03-05 13:34:26 +0000

    Add and use mikro-orm config to be used on the CLI

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,17 @@
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants"
 import { Post } from "./entities/Post"
+import mikroOrmConfig from "./mikro-orm.config"
 
 const main = async () => {
-    const orm = await MikroORM.init({
-        entities: [Post],
-        dbName: 'reddit',
-        type: 'postgresql',
-        debug: !__prod__,
+    const orm = await MikroORM.init(mikroOrmConfig)
+
+    const post = orm.em.create(Post, {
+        title: 'first post'
     })
+    await orm.em.persistAndFlush(post)
 }
 
-main()
+main().catch(error => {
+    console.log(error)
+})
