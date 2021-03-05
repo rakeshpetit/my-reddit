eb8dd6547929b1bb404cc9bdf3c3704175ee8a40
Rakesh Arunachalam <rakesh@thoughtbot.com>
2021-03-05 14:52:42 +0000

    Add ESLint for TS and autoformat on save

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -4,12 +4,12 @@ import { MikroORM } from '@mikro-orm/core'
 import path from 'path'
 
 export default {
-    migrations: {
-        path: path.join(__dirname, './migrations'),
-        pattern: /^[\w-]+\d+\.[tj]s$/,
-    },
-    entities: [Post],
-    dbName: 'reddit',
-    type: 'postgresql',
-    debug: !__prod__,
+  migrations: {
+    path: path.join(__dirname, './migrations'),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post],
+  dbName: 'reddit',
+  type: 'postgresql',
+  debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0]
