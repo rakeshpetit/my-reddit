8271133c0b45928f1444343912ade7cc7f697c7e
Rakesh Arunachalam <rakesh@thoughtbot.com>
2021-03-05 16:28:38 +0000

    Create a Post resolver to return array of posts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
+import 'reflect-metadata'
 import { MikroORM } from "@mikro-orm/core"
 import { ApolloServer } from "apollo-server-express"
 import express from "express"
 import { buildSchema } from "type-graphql"
 import mikroOrmConfig from "./mikro-orm.config"
 import { HelloResolver } from "./resolvers/hello"
+import { PostResolver } from "./resolvers/post"
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig)
@@ -13,9 +15,10 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false
-    })
+    }),
+    context: () => ({ em: orm.em })
   })
 
   apolloServer.applyMiddleware({ app })
