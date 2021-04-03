ea1c97f2ae2da753b044d05a99afa84bf0fab305
Rakesh Arunachalam <rakesh@thoughtbot.com>
2021-04-03 13:30:24 +0100

    Paginate and load more posts based on limits

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -16,7 +16,8 @@ const Index = () => {
     variables,
   });
 
-  if (!fetching && !data) {
+
+  if (!fetching && (!data?.posts)) {
     return <div>Your query failed for some reason</div>;
   }
 
@@ -32,7 +33,7 @@ const Index = () => {
         <div>Loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data!.posts.map((p) => (
+          {data!.posts.posts.map((p) => (
             <Box p={5} key={p.id} shadow="md" borderWidth="1px">
               <Heading fontSize="xl">{p.title}</Heading>
               <Text mt={4}>{p.textSnippet}</Text>
@@ -40,13 +41,13 @@ const Index = () => {
           ))}
         </Stack>
       )}
-      {data && (
+      {data && data.posts.hasMore && (
         <Flex mt="4">
           <Button
             onClick={() => {
               setVariables({
                 limit: variables.limit,
-                cursor: data.posts[data.posts.length - 1].createdAt,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
               });
             }}
             backgroundColor="messenger.100"
