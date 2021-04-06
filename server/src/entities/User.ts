6a23256ec1b29e19ab29551ff1c5f50941d1a5c4
Rakesh Arunachalam <rakesh@thoughtbot.com>
2021-04-06 20:39:31 +0100

    Updoot entity and relationship with Post and User

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,6 +1,7 @@
-import { Post } from './Post';
 import { Field, ObjectType } from "type-graphql";
 import { BaseEntity, Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Post } from './Post';
+import { Updoot } from './Updoot';
 
 @ObjectType()
 @Entity()
@@ -18,6 +19,9 @@ export class User extends BaseEntity {
 
     @OneToMany(() => Post, post => post.creator)
     posts: Post[]
+
+    @OneToMany(() => Updoot, updoot => updoot.user)
+    updoots: Updoot[]
 
     @Field(() => String)
     @CreateDateColumn()
