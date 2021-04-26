d79b7080c3e1daf1a3d94a8f50b58f1d7c6f7ba5
Rakesh Arunachalam <rakesh@thoughtbot.com>
2021-04-26 21:15:22 +0100

    Upvote and Downvote functionality works

--- web/src/components/UpdootSection.tsx
+++ web/src/components/UpdootSection.tsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Flex, IconButton } from "@chakra-ui/react";
 import { ChevronUpIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import Wrapper from "./Wrapper";
-import { PostSnippetFragment } from "../generated/graphql";
+import { PostSnippetFragment, useVoteMutation } from "../generated/graphql";
 
 interface UpdootSectionProps {
   post: PostSnippetFragment;
 }
 
 export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
+  const [, vote] = useVoteMutation();
+  const [loadingState, setLoadingState] = useState<
+    "upvote-loading" | "downvote-loading" | "not-loading"
+  >("not-loading");
   return (
     <Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
-      <IconButton onClick={() => {}} aria-label="Upvote post">
+      <IconButton
+        onClick={async () => {
+          setLoadingState("upvote-loading");
+          await vote({
+            postId: post.id,
+            value: 1,
+          });
+          setLoadingState("not-loading");
+        }}
+        aria-label="Upvote post"
+      >
         <ChevronUpIcon />
       </IconButton>
       {post.points}
-      <IconButton onClick={() => {}} aria-label="Downvote post">
+      <IconButton
+        onClick={async () => {
+          setLoadingState("downvote-loading");
+          await vote({
+            postId: post.id,
+            value: -1,
+          });
+          setLoadingState("not-loading");
+        }}
+        aria-label="Downvote post"
+      >
         <ChevronDownIcon />
       </IconButton>
     </Flex>
